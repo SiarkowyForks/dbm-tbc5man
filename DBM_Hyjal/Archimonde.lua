7047318d8c150c91a2eee68d390e613088b27ec0
Yatzii <47720837+Yatzii93@users.noreply.github.com>
2019-11-20 05:31:36 +0000

    Archimonde Enrage Timer Update
    m_uiEnrageTimer          = 7 * MINUTE * IN_MILLISECONDS;

--- DBM_Hyjal/Archimonde.lua
+++ DBM_Hyjal/Archimonde.lua
@@ -21,12 +21,12 @@ Archimonde:AddBarOption("Enrage")
 Archimonde:AddBarOption("Fear")
 
 function Archimonde:OnCombatStart(delay)
-	self:StartStatusBarTimer(600 - delay, "Enrage", "Interface\\Icons\\Spell_Shadow_UnholyFrenzy");
-	self:ScheduleSelf(300 - delay, "EnrageWarn", 300);
-	self:ScheduleSelf(480 - delay, "EnrageWarn", 120);
-	self:ScheduleSelf(540 - delay, "EnrageWarn", 60);
-	self:ScheduleSelf(570 - delay, "EnrageWarn", 30);
-	self:ScheduleSelf(590 - delay, "EnrageWarn", 10);
+	self:StartStatusBarTimer(420 - delay, "Enrage", "Interface\\Icons\\Spell_Shadow_UnholyFrenzy");
+	self:ScheduleSelf(210 - delay, "EnrageWarn", 210);
+	self:ScheduleSelf(310 - delay, "EnrageWarn", 100);
+	self:ScheduleSelf(370 - delay, "EnrageWarn", 60);
+	self:ScheduleSelf(400 - delay, "EnrageWarn", 30);
+	self:ScheduleSelf(410 - delay, "EnrageWarn", 10);
 	
 	self:StartStatusBarTimer(40 - delay, "Fear", "Interface\\Icons\\Spell_Shadow_PsychicScream")
 	self:ScheduleSelf(37 - delay, "FearWarn")
