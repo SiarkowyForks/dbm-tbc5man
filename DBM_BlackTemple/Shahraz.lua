cce3cd16d92512dbd28da411e9eda4c01de9aec4
Yatzii <47720837+Yatzii93@users.noreply.github.com>
2019-11-20 05:42:44 +0000

    Mother: Update Enrage timers
    m_uiBerserkTimer            = 8 * MINUTE * IN_MILLISECONDS;
    
    Mother: Suppress older clients, add credit
    
    Mother: Revert the original 5 min enrage warning

--- DBM_BlackTemple/Shahraz.lua
+++ DBM_BlackTemple/Shahraz.lua
@@ -1,7 +1,7 @@
 local Shahraz = DBM:NewBossMod("Shahraz", DBM_SHAHRAZ_NAME, DBM_SHAHRAZ_DESCRIPTION, DBM_BLACK_TEMPLE, DBM_BT_TAB, 7)
 
-Shahraz.Version	= "1.0"
-Shahraz.Author	= "Tandanu"
+Shahraz.Version	= "1.1"
+Shahraz.Author	= "Tandanu, Yatzii"
 
 Shahraz:RegisterCombat("YELL", DBM_SHAHRAZ_YELL_PULL)
 
@@ -20,12 +20,12 @@ Shahraz:RegisterEvents(
 )
 
 function Shahraz:OnCombatStart(delay)
-	self:StartStatusBarTimer(600 - delay, "Enrage", "Interface\\Icons\\Spell_Shadow_UnholyFrenzy")
-	self:ScheduleSelf(300 - delay, "EnrageWarn", 300)
-	self:ScheduleSelf(480 - delay, "EnrageWarn", 120)
-	self:ScheduleSelf(540 - delay, "EnrageWarn", 60)
-	self:ScheduleSelf(570 - delay, "EnrageWarn", 30)
-	self:ScheduleSelf(590 - delay, "EnrageWarn", 10)
+	self:StartStatusBarTimer(480 - delay, "Enrage", "Interface\\Icons\\Spell_Shadow_UnholyFrenzy")
+	self:ScheduleSelf(180 - delay, "EnrageWarn", 300)
+	self:ScheduleSelf(360 - delay, "EnrageWarn", 120)
+	self:ScheduleSelf(420 - delay, "EnrageWarn", 60)
+	self:ScheduleSelf(450 - delay, "EnrageWarn", 30)
+	self:ScheduleSelf(470 - delay, "EnrageWarn", 10)
 	
 	fa = {}
 	icon = 8
