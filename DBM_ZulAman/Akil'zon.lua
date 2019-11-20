200c512270bf1bf18e4fd40cc23d5519a7de6532
Yatzii <47720837+Yatzii93@users.noreply.github.com>
2019-11-20 05:18:10 +0000

    Update Akil'zon Storm Timers.
    Inital Timers:
    m_uiStaticDisruptTimer  = urand(7000, 14000);
    m_uiCallLightTimer      = urand(15000, 25000);
    m_uiGustOfWindTimer     = urand(20000, 30000);
    m_uiStormTimer          = 40000;
    m_uiSummonEagleTimer    = 55000;
    m_uiBerserkTimer        = 10 * MINUTE * IN_MILLISECONDS;
    
    After use: (no changes)
    m_uiStormTimer = 40000;

--- DBM_ZulAman/Akil'zon.lua
+++ DBM_ZulAman/Akil'zon.lua
@@ -21,8 +21,8 @@ function Akilzon:OnCombatStart()
 	self:ScheduleAnnounce(570, DBM_GENERIC_ENRAGE_WARN:format(30, DBM_SEC), 3)
 	self:ScheduleAnnounce(590, DBM_GENERIC_ENRAGE_WARN:format(10, DBM_SEC), 4)
 	
-	self:StartStatusBarTimer(49, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
-	self:ScheduleAnnounce(44, DBM_AKIL_WARN_STORM_SOON, 1)
+	self:StartStatusBarTimer(39, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
+	self:ScheduleAnnounce(34, DBM_AKIL_WARN_STORM_SOON, 1)
 	if self.Options.RangeCheck then
 		DBM_Gui_DistanceFrame_Show()
 	end
@@ -52,8 +52,8 @@ function Akilzon:OnSync(msg)
 		msg = msg:sub(6)
 		self:Announce(DBM_AKIL_WARN_STORM_ON:format(msg), 2)
 		self:SetIcon(msg, 10)
-		self:StartStatusBarTimer(56, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
-		self:ScheduleAnnounce(51, DBM_AKIL_WARN_STORM_SOON, 1)
+		self:StartStatusBarTimer(41, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
+		self:ScheduleAnnounce(36, DBM_AKIL_WARN_STORM_SOON, 1)
 		if msg == UnitName("player") then
 			Minimap:PingLocation(CURSOR_OFFSET_X, CURSOR_OFFSET_Y)
 		end
