429e0e62b2fffddef197f1d6ee81eb20cb33f1da
Yatzii <47720837+Yatzii93@users.noreply.github.com>
2019-11-20 05:18:10 +0000

    Akil'zon: Update Storm timers
    Inital Timers:
    m_uiStaticDisruptTimer  = urand(7000, 14000);
    m_uiCallLightTimer      = urand(15000, 25000);
    m_uiGustOfWindTimer     = urand(20000, 30000);
    m_uiStormTimer          = 40000;
    m_uiSummonEagleTimer    = 55000;
    m_uiBerserkTimer        = 10 * MINUTE * IN_MILLISECONDS;
    
    After use: (no changes)
    m_uiStormTimer = 40000;

--- DBM_ZulAman/Akil'zon.lua
+++ DBM_ZulAman/Akil'zon.lua
@@ -1,7 +1,7 @@
 local Akilzon = DBM:NewBossMod("Akilzon", DBM_AKIL_NAME, DBM_AKIL_DESCRIPTION, DBM_ZULAMAN, DBM_ZULAMAN_TAB, 2);
 
-Akilzon.Version		= "1.0";
-Akilzon.Author		= "Tandanu";
+Akilzon.Version		= "1.1";
+Akilzon.Author		= "Tandanu, Yatzii";
 
 Akilzon:RegisterEvents(
 	"SPELL_AURA_APPLIED"
@@ -13,16 +13,16 @@ Akilzon:AddOption("RangeCheck", true, DBM_AKIL_OPTION_RANGE)
 Akilzon:AddBarOption("Enrage")
 Akilzon:AddBarOption("Electrical Storm")
 
-function Akilzon:OnCombatStart()
-	self:StartStatusBarTimer(600, "Enrage", "Interface\\Icons\\Spell_Shadow_UnholyFrenzy") 
-	self:ScheduleAnnounce(300, DBM_GENERIC_ENRAGE_WARN:format(5, DBM_MIN), 1)
-	self:ScheduleAnnounce(420, DBM_GENERIC_ENRAGE_WARN:format(3, DBM_MIN), 1)
-	self:ScheduleAnnounce(540, DBM_GENERIC_ENRAGE_WARN:format(1, DBM_MIN), 2)
-	self:ScheduleAnnounce(570, DBM_GENERIC_ENRAGE_WARN:format(30, DBM_SEC), 3)
-	self:ScheduleAnnounce(590, DBM_GENERIC_ENRAGE_WARN:format(10, DBM_SEC), 4)
+function Akilzon:OnCombatStart(delay)
+	self:StartStatusBarTimer(600 - delay, "Enrage", "Interface\\Icons\\Spell_Shadow_UnholyFrenzy")
+	self:ScheduleAnnounce(300 - delay, DBM_GENERIC_ENRAGE_WARN:format(5, DBM_MIN), 1)
+	self:ScheduleAnnounce(420 - delay, DBM_GENERIC_ENRAGE_WARN:format(3, DBM_MIN), 1)
+	self:ScheduleAnnounce(540 - delay, DBM_GENERIC_ENRAGE_WARN:format(1, DBM_MIN), 2)
+	self:ScheduleAnnounce(570 - delay, DBM_GENERIC_ENRAGE_WARN:format(30, DBM_SEC), 3)
+	self:ScheduleAnnounce(590 - delay, DBM_GENERIC_ENRAGE_WARN:format(10, DBM_SEC), 4)
 	
-	self:StartStatusBarTimer(49, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
-	self:ScheduleAnnounce(44, DBM_AKIL_WARN_STORM_SOON, 1)
+	self:StartStatusBarTimer(40 - delay, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
+	self:ScheduleAnnounce(35 - delay, DBM_AKIL_WARN_STORM_SOON, 1)
 	if self.Options.RangeCheck then
 		DBM_Gui_DistanceFrame_Show()
 	end
@@ -52,8 +52,8 @@ function Akilzon:OnSync(msg)
 		msg = msg:sub(6)
 		self:Announce(DBM_AKIL_WARN_STORM_ON:format(msg), 2)
 		self:SetIcon(msg, 10)
-		self:StartStatusBarTimer(56, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
-		self:ScheduleAnnounce(51, DBM_AKIL_WARN_STORM_SOON, 1)
+		self:StartStatusBarTimer(40, "Electrical Storm", "Interface\\Icons\\Spell_Lightning_LightningBolt01")
+		self:ScheduleAnnounce(35, DBM_AKIL_WARN_STORM_SOON, 1)
 		if msg == UnitName("player") then
 			Minimap:PingLocation(CURSOR_OFFSET_X, CURSOR_OFFSET_Y)
 		end
